9ed9113c0852e5f063e62f6dc811ff8614531346
harrier999 <harrier999@naver.com>
2022-02-25 06:40:42 +0000

    FIX random.choice -> random.sample

--- code/legacy_models/v2/dataset.py
+++ code/legacy_models/v2/dataset.py
@@ -252,7 +252,7 @@ class MaskSplitByProfileDataset(MaskBaseDataset):
         length = len(profiles)
         n_val = int(length * val_ratio)
 
-        val_indices = set(random.choices(range(length), k=n_val))
+        val_indices = set(random.sample(range(length), k=n_val)) # random.choice(range(length), k=n_val)는 비복원 추출
         train_indices = set(range(length)) - val_indices
         return {
             "train": train_indices,
